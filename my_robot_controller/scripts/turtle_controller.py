95f8d627b93a5cda5c7b9a18d53cebbc386d795a
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2022-12-11 13:29:26 +0530

    write a service client

--- my_robot_controller/scripts/turtle_controller.py
+++ my_robot_controller/scripts/turtle_controller.py
@@ -2,6 +2,22 @@
 import rospy
 from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
+from turtlesim.srv import SetPen
+
+previous_x = 0
+
+def call_set_pen_service(r, g, b, width, off):
+    try:
+        # arg1-name of srv: rosservice list, 
+        # arg2-type of srv: rosservice info /turtle1/set_pen-> Type: turtlesim/SetPen
+        set_pen = rospy.ServiceProxy('/turtle1/set_pen', SetPen)
+        
+        # Args: r g b width off
+        response = set_pen(r, g, b, width, off)
+        # rospy.loginfo("SetPen response: ", response)
+
+    except rospy.ServiceException as e:
+        rospy.logwarn(e)
 
 
 def pose_callback(pose: Pose):
@@ -19,11 +35,29 @@ def pose_callback(pose: Pose):
 
     pub.publish(cmd)
 
+    # change color of the pen depending on the region
+    global previous_x
+
+    if pose.x >=5.5 and previous_x <5.5:
+        
+        rospy.loginfo("Set color to red")
+        call_set_pen_service(255, 0, 0, 4, 0)
+
+    elif pose.x < 5.5 and previous_x >=5.5:
+        
+        call_set_pen_service(0, 255, 0, 4, 0)
+        rospy.loginfo("Set color to green")
+    
+    previous_x = pose.x
 
 if __name__ == '__main__':
     
     rospy.init_node('turtle_controller')
     rospy.loginfo('Running turtle controller')
+
+    # wait for a service to start
+    # use: rosservice list to get the list of services for a given node
+    rospy.wait_for_service("/turtle1/set_pen")
 
     # publisher
     pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=10)
@@ -35,3 +69,33 @@ if __name__ == '__main__':
 
     # passive loop
     rospy.spin()
+
+
+#      bimalka98@LAP-BIMALKA98:~/ros/yt_ros_ws$ rosservice list 
+#     /clear
+#     /kill
+#     /reset
+#     /rosout/get_loggers
+#     /rosout/set_logger_level
+#     /spawn
+#     /turtle1/set_pen
+#     /turtle1/teleport_absolute
+#     /turtle1/teleport_relative
+#     /turtlesim/get_loggers
+#     /turtlesim/set_logger_level
+
+#     bimalka98@LAP-BIMALKA98:~/ros/yt_ros_ws$ rosservice info /turtle1/set_pen 
+#     Node: /turtlesim
+#     URI: rosrpc://LAP-BIMALKA98:44757
+#     Type: turtlesim/SetPen
+#     Args: r g b width off
+    
+#     bimalka98@LAP-BIMALKA98:~/ros/yt_ros_ws$ rossrv show turtlesim/SetPen
+#     uint8 r
+#     uint8 g
+#     uint8 b
+#     uint8 width
+#     uint8 off
+#     ---
+
+#     bimalka98@LAP-BIMALKA98:~/ros/yt_ros_ws$ 
